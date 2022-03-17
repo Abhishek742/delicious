541d79d9036d4c41988ef653b6c7462a21ecc41d
Abhishek742 <abhishekdurairaj2001@gmail.com>
2022-03-17 12:58:59 +0530

    framer motion

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-const Grid = styled.div`
+import { motion } from "framer-motion";
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -26,7 +26,12 @@ function Cuisine() {
   };
 
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <GridCard key={item.id}>
